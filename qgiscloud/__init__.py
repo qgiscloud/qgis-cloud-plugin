464c2b06545cb65326fd0e3545e0c9573026a9b8
Pirmin Kalberer <pka@sourcepole.ch>
2013-02-11 22:51:37 +0100

    Version 0.8.2

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "0.8.1"
+    return "0.8.2"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
