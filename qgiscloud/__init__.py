5358c6c319bf51e637a5aa42958e36d1b4b34044
hdus <horst.duester@sourcepole.ch>
2014-09-16 16:16:03 +0200

    correct revision 0.11.15 set

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "0.11.14"
+    return "0.11.15"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
