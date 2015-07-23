83d4939b94e2859a92e97859c1d49701f6ad6caf
Sandro Mani <manisandro@gmail.com>
2015-07-23 13:23:18 +0200

    Fix yet another struct.pack integer overflow issue

--- qgiscloud/data_upload.py
+++ qgiscloud/data_upload.py
@@ -221,7 +221,7 @@ class DataUpload(QObject):
     def _wkbToEWkbHex(self, wkb, srid, convertToMulti=False):
         wktType = struct.unpack("=i", wkb[1:5])[0] & 0xffffffff
         if not QGis.isMultiType(wktType):
-            wktType = QGis.multiType(wktType)
+            wktType = QGis.multiType(wktType) & 0xffffffff
             wkb = wkb[0] + struct.pack("=I", wktType) + struct.pack("=I", 1) + wkb
 
         # See postgis sources liblwgeom.h.in:
