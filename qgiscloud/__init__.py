2a2946d0a6e115d57a3c715ee5c6c27f112709aa
Sandro Mani <manisandro@gmail.com>
2015-08-04 17:29:22 +0200

    Bump version to 1.1.7

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "1.1.6"
+    return "1.1.7"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
