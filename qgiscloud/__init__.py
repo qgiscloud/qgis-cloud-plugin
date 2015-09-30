a406930be01e847ed797efe6d6737e5073579150
Horst Duester <horst.duester@sourcepole.ch>
2015-09-30 14:12:30 +0200

    set to revision 1.1.10

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "1.1.9"
+    return "1.1.10"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
