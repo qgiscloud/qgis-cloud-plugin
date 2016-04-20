38507d545760e9ffdbf8e9aee6f87828e9b92caa
Horst Duester <horst.duester@sourcepole.ch>
2016-04-20 14:11:50 +0200

    set to release 1.3.1

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "1.3.0"
+    return "1.3.1"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
