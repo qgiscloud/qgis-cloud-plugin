6d6c648f55e27ef0be073dfa64d690cfb466ec40
Horst Duester <hdus@sourcepole.ch>
2017-08-04 11:01:29 +0200

    Upload encoding issue (Windows) fixed, set to revision 1.4.14

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps on qgiscloud.com"
 def version():
-    return "1.4.13"
+    return "1.4.14"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
