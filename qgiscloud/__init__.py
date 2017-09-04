82bb286ec601af54e7e8f84897db7f9e02ce73dc
Horst Duester <horst.duester@sourcepole.ch>
2017-09-04 11:29:21 +0200

    metadata improved

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps and share data on qgiscloud.com"
 def version():
-    return "1.4.16"
+    return "1.4.17"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
