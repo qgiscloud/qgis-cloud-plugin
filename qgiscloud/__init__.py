56ee1eb5a84ba8452313ff2e9b53ed3db47162d2
Horst Düster <hdus@sourcepole.ch>
2018-05-23 14:15:45 +0200

    set to revision 1.5.14

--- qgiscloud/__init__.py
+++ qgiscloud/__init__.py
@@ -25,7 +25,7 @@ def name():
 def description():
     return "Publish maps and share data on qgiscloud.com"
 def version():
-    return "1.5.13"
+    return "1.5.14"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
