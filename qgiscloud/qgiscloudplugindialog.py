acce038e61c27a05bae4c7fafa3ae67ec1eda7fb
Horst Düster <hdus@sourcepole.ch>
2018-06-06 09:35:39 +0200

    lblWebMap missing trailing / added

--- qgiscloud/qgiscloudplugindialog.py
+++ qgiscloud/qgiscloudplugindialog.py
@@ -550,7 +550,7 @@ class QgisCloudPluginDialog(QDockWidget):
             pass
             
         self.update_url(self.ui.lblWebmap, self.api_url(),
-                        'https://', u'{0}/{1}'.format(self.user, map))
+                        'https://', u'{0}/{1}/'.format(self.user, map))
                         
         if self.clouddb:
             self.update_url(
