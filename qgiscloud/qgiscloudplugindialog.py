bedc6897a2f7080988dad909cdd3517fa6e13b36
Horst <hdus@sourcepole.ch>
2018-08-16 10:43:00 +0200

    QGIS private cloud missing tos_accepted handled

--- qgiscloud/qgiscloudplugindialog.py
+++ qgiscloud/qgiscloudplugindialog.py
@@ -296,21 +296,25 @@ class QgisCloudPluginDialog(QDockWidget):
                     login_info = self.api.check_login(
                         version_info=self._version_info())
 
-                    if not login_info['tos_accepted']:
-                        result = QMessageBox.information(
-                            None,
-                            self.tr("Accept new Privacy Policy"),
-                            self.tr("""Due to the GDPR qgiscloud.com has a new <a href='http://qgiscloud.com/en/pages/privacy'> Privacy Policy </a>. 
-                            To continue using qgiscloud.com, you must accept the new policy. """),
-                            QMessageBox.StandardButtons(
-                                QMessageBox.No |
-                                QMessageBox.Yes))
-                        
-                        if result == QMessageBox.No:
-                            login_ok = False
-                            return
-                        else:
-                            result = self.api.accept_tos()
+# QGIS private Cloud has no tos_accepted
+                    try:
+                        if not login_info['tos_accepted']:
+                            result = QMessageBox.information(
+                                None,
+                                self.tr("Accept new Privacy Policy"),
+                                self.tr("""Due to the GDPR qgiscloud.com has a new <a href='http://qgiscloud.com/en/pages/privacy'> Privacy Policy </a>. 
+                                To continue using qgiscloud.com, you must accept the new policy. """),
+                                QMessageBox.StandardButtons(
+                                    QMessageBox.No |
+                                    QMessageBox.Yes))
+                            
+                            if result == QMessageBox.No:
+                                login_ok = False
+                                return
+                            else:
+                                result = self.api.accept_tos()
+                    except:
+                        pass
                             
                     self.user = login_dialog.ui.editUser.text()
                     self._update_clouddb_mode(login_info['clouddb'])
