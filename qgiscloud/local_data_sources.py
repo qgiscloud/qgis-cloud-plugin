a2cc34cf23c21dda7470d5e50449f3db73adf31d
Horst <horst.duester@sourcepole.ch>
2024-02-20 09:12:32 +0100

    support of xyzvectortiles starting with QGIS 3.34, due to providername has been hanged

--- qgiscloud/local_data_sources.py
+++ qgiscloud/local_data_sources.py
@@ -82,7 +82,7 @@ class LocalDataSources(object):
                 else:
                     local_raster_layers.append(layer)
 
-            elif provider not in ["wms", "openlayers",  "arcgismapserver",   "postgresraster",  "vectortile"]:
+            elif provider not in ["wms", "openlayers",  "arcgismapserver",   "postgresraster",  "vectortile",  "xyzvectortiles"]:
                 if layer.type() == QgsMapLayer.VectorLayer:
                     local_layers.append(layer)
                 else:
